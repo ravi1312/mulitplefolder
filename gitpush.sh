384eed88c3f2a812ba03906d336f0c8d4e579d88
ravi1312 <63062820+ravi1312@users.noreply.github.com>
2020-08-03 19:16:38 +0530

    Update gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -3,15 +3,15 @@ commit=$1
 token=$2
 v=`echo "$commit"| awk '{print $1}'`
 echo "$v"
-array=($(echo $commit | awk '{gsub(",","\n")} 1' | awk '{print $1}'))
-echo "$array"
+#array=($(echo $commit | awk '{gsub(",","\n")} 1' | awk '{print $1}'))
+#echo "$array"
 #echo "git pull https://$token@github.com/thermofisher/tfsds-Databricks-Notebook-Deploy-POC.git"
 #echo "git cherry-pick $v"
 #echo "git push https://$token@github.com/thermofisher/tfsds-Databricks-Notebook-Deploy-POC.git"
  
 
 array=$(echo $commit | awk '{gsub(",","\n")} 1' | awk '{print $1}')
-
+echo "$array"
 for i in `echo $array`
 
 do
