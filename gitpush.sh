2602cfdc8c84e3ab94dae75dcb5ba3b353556b5d
ravi1312 <63062820+ravi1312@users.noreply.github.com>
2020-08-03 19:14:20 +0530

    Update gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -5,7 +5,22 @@ v=`echo "$commit"| awk '{print $1}'`
 echo "$v"
 array=($(echo $commit | awk '{gsub(",","\n")} 1' | awk '{print $1}'))
 echo "$array"
-echo "git pull https://$token@github.com/thermofisher/tfsds-Databricks-Notebook-Deploy-POC.git"
-echo "git cherry-pick $v"
-echo "git push https://$token@github.com/thermofisher/tfsds-Databricks-Notebook-Deploy-POC.git"
-echo "hello"
+#echo "git pull https://$token@github.com/thermofisher/tfsds-Databricks-Notebook-Deploy-POC.git"
+#echo "git cherry-pick $v"
+#echo "git push https://$token@github.com/thermofisher/tfsds-Databricks-Notebook-Deploy-POC.git"
+ 
+
+array=$(echo $commit | awk '{gsub(",","\n")} 1' | awk '{print $1}')
+
+for i in `echo $array`
+
+do
+
+  echo $i
+
+  git cherry-pick $i
+
+  #git push https://$token@github.com/thermofisher/tfsds-databricks-ci-test-2.git
+
+done
+git push https://$token@github.com/thermofisher/tfsds-databricks-ci-test-2.git
